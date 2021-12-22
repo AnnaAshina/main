15aedaf7c66d632c1770ba8cdeef68d46e3e8d62
AnnaAshina <sdk-a@yandex.ru>
2021-12-22 18:04:11 +0400

    add zadacha 3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()??"0");
+int i = 0;
+
+while (i <= number*2)
+{
+    int k = i - number;
+    Console.Write(k +" " );   
+    i = i + 1;
+}

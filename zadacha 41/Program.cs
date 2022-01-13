637ec5c9a6942565c92236511b2867eea8b2a83c
AnnaAshina <sdk-a@yandex.ru>
2022-01-13 11:54:50 +0400

    add zadacha 41

---
+++ zadacha 41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+Console.WriteLine($"Введите число 1 ");
+int number1 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"Введите число 2 ");
+int number2 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"Введите число 3 ");
+int number3 = int.Parse(Console.ReadLine()??"0");
+if((number1 + number2 > number3) && (number1 + number3 > number2) && (number2 + number3 > number1))
+    Console.WriteLine("да");
+else Console.WriteLine("нет");   
+
+
+

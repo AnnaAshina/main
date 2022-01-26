37bc00455641e0447550e690e891f8132aa334da
AnnaAshina <sdk-a@yandex.ru>
2022-01-27 00:33:35 +0400

    add zad43

---
+++ zadacha43/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+int[,] matrix = new int [m,n];
+Console.WriteLine("Введите переменные m и n: ");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+int[] CreateArray(int m,n);
+
+
+
+
+
